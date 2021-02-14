a79a3494f165e2b2e89a5346bee15a38aaf264b0
Andreas Wälchli <andreas.waelchli@me.com>
2021-02-14 22:21:12 +0000

    role based method security. Also removed user role.

---
+++ backend/src/main/java/ch/awae/mytools/security/MethodSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package ch.awae.mytools.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class MethodSecurityConfiguration {
+}
---
+++ backend/src/main/resources/db/migration/V3__remove_user_role.sql
@@ -0,0 +1,1 @@
+delete from user_roles where role = 'USER';
